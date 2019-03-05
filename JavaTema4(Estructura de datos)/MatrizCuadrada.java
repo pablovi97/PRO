49cbf8f9e88475fe0ab2b87764e3655b46410730
Pablo Viera <32093629+pablovi97@users.noreply.github.com>
2019-03-05 13:29:59 +0000

    cambio usando bucles

--- JavaTema4(Estructura de datos)/MatrizCuadrada.java
+++ JavaTema4(Estructura de datos)/MatrizCuadrada.java
@@ -50,3 +50,86 @@ compotenente es la suma de las componentes
     }
     
 }
+
+/*
+OTRA FORMA
+
+public class Practica6d5 {
+
+    public static void main(String[] args) {
+        /*
+        Práctica 6: Emular la suma de dos matrices. Se creará una clase: MatrizCuadrada con un
+constructor que reciba el tamaño en filas de la matriz, digamos: n. Entonces el constructor
+establece como atributo un array de n*n Se deberá crear un método llamado:
+MatrizCuadrada suma(MatrizCuadrada) que sirve para hacer la suma de dos matrices
+devolviendo la matriz suma ( la suma de dos matrices es una nueva matriz donde cada
+compotenente es la suma de las componentes 
+         */
+        
+        
+        MatrizCuadrada mc = new MatrizCuadrada(4);
+        MatrizCuadrada mc1 = new MatrizCuadrada(4);
+        
+        
+       mostrarArray(mc.suma(mc, mc));
+
+    }
+     public static void  mostrarArray( int[][]arr){
+        for (int[] is : arr) {
+            for (int i : is) {
+                System.out.print(i +" ");
+            }
+            System.out.println("");
+        }
+    }
+
+}
+
+class MatrizCuadrada {
+
+    int[][] arr;
+
+    public MatrizCuadrada(int n) {
+        filas = n;
+        arr = new int[n][n];
+           Random rnd = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            int[] fila = arr[i];
+            for (int j = 0; j < fila.length; j++) {
+               fila[j]=rnd.nextInt((99-1)+1)+1;
+                
+            }
+            
+        }
+        
+
+    }
+    
+    
+    public int[][] suma(MatrizCuadrada c ,MatrizCuadrada k){
+        
+        int [][] newarr = new int[this.filas][this.filas];
+        
+        for (int i = 0; i < arr.length; i++) {
+            int[] is = arr[i];
+            for (int j = 0; j < is.length; j++) {
+                int sum = c.arr[i][j] +k.arr[i][j];
+                
+                newarr[i][j]=sum;
+                
+            }
+            
+        }
+        
+        
+        
+        return newarr;
+        
+    }
+    
+    
+    
+
+    int filas;
+}
+*/
